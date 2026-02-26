a4761b7744540260105a0b27aecdf0d3b2e665ae
baskak <andrbaskov@gmail.com>
2026-02-26 22:07:32 +0500

    Add middleware for response headers Baskov a

---
+++ TaskHub/Api/Middleware/ResponseTimeMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace Api.Middleware;
+
+public class ResponseTimeMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ResponseTimeMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        context.Response.OnStarting(() =>
+        {
+            stopwatch.Stop();
+            context.Response.Headers["X-Response-Time-Ms"] = stopwatch.ElapsedMilliseconds.ToString();
+            return Task.CompletedTask;
+        });
+
+        await _next(context);
+    }
+}
---
+++ TaskHub/Api/Middleware/StudentInfoMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace Api.Middleware;
+
+public class StudentInfoMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public StudentInfoMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context.Response.OnStarting(() =>
+        {
+            context.Response.Headers["X-Student-Group"] = "PU-240930";
+            context.Response.Headers["X-Student-Name"] = "Baskov Andrei Alekseevich";
+            return Task.CompletedTask;
+        });
+        await _next(context);
+    }
+}
