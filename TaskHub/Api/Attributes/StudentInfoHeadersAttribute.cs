1d2b0010153c90d868a7e899bb16af5bd3e38bc7
baskak <andrbaskov@gmail.com>
2026-03-11 21:25:30 +0500

    attributes

---
+++ TaskHub/Api/Attributes/StudentInfoHeadersAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Atributes;
+
+public class StudentInfoHeadersAttribute : ActionFilterAttribute
+{
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        context.HttpContext.Response.OnStarting(() =>
+        {
+            context.HttpContext.Response.Headers["X-Student-Group"] = "PU-240930";
+            context.HttpContext.Response.Headers["X-Student-Name"] = "Baskov Andrei Alekseevich";
+            return Task.CompletedTask;
+        });
+        await next();
+    }
+}
---
+++ TaskHub/Api/Attributes/ValidateUserRequestAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+
+namespace Api.Atributes;
+
+public class ValidateUserRequestAttribute : ActionFilterAttribute
+{
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        object request = null;
+        foreach (var arg in context.ActionArguments.Values)
+        {
+            if (arg != null && arg.GetType().GetProperty("Name") != null)
+            {
+                request = arg;
+                break;
+            }
+        }
+        if (request == null)
+        {
+            context.Result = new BadRequestObjectResult("Тело запроса отсутствует");
+            return;
+        }
+        var nameProperty = request.GetType().GetProperty("Name");
+        var nameValue = nameProperty?.GetValue(request) as string;
+        if (string.IsNullOrWhiteSpace(nameValue))
+        {
+            context.Result = new BadRequestObjectResult("Имя пользователя не задано");
+            return;
+        }
+        await next();
+    }
+}
