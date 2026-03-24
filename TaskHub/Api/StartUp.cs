1344eaa12e2efed789d583b99c8330b32936cfa2
baskak <andrbaskov@gmail.com>
2026-03-24 14:25:29 +0500

    Add FromRouteTaskIdAttribute

--- TaskHub/Api/StartUp.cs
+++ TaskHub/Api/StartUp.cs
@@ -1,3 +1,4 @@
+
 using Api.UseCases.Users;
 using Api.UseCases.Users.Interfaces;
 using Api.UseCases.Tasks;
@@ -43,7 +44,10 @@ public sealed class Startup
     /// <param name="services">Коллекция сервисов</param>
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddControllers();
+        services.AddControllers(options =>
+        {
+            options.ModelBinderProviders.Insert(0, new FromRouteTaskIdAttribute());
+        });
         services.AddDal();
         services.AddLogic();
         services.AddSingleton<Singleton1, Singleton1>();
@@ -52,21 +56,11 @@ public sealed class Startup
         services.AddScoped<Scoped2, Scoped2>();
         services.AddTransient<Transient1, Transient1>();
         services.AddTransient<Transient2, Transient2>();
-
-        // 👇 ДОБАВЛЯЕМ РЕГИСТРАЦИЮ БАЗЫ ДАННЫХ
         services.AddDbContext<TaskDbContext>(options =>
             options.UseNpgsql(Configuration.GetConnectionString("Postgres")));
-
-        // 👇 ДОБАВЛЯЕМ РЕГИСТРАЦИЮ РЕПОЗИТОРИЯ
         services.AddScoped<ITaskRepository, TaskRepository>();
-
-        // 👇 ДОБАВЛЯЕМ РЕГИСТРАЦИЮ СЕРВИСА
         services.AddScoped<ITaskService, TaskService>();
-
-        // UseCases для пользователей
         services.AddScoped<IManageUserUseCase, ManageUserUseCase>();
-
-        // UseCases для задач
         services.AddScoped<IManageTaskUseCase, ManageTaskUseCase>();
 
         services.AddCors(options =>
